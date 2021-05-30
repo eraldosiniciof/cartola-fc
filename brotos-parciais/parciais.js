7cdbd3259d93da02304d86913a4de47f04c0f397
eraldosiniciof <eraldosinicio@gmail.com>
2021-05-29 23:14:29 -0300

    atualizacoes 2021

---
+++ brotos-parciais/parciais.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+
+let urlParciais = 'http://api.cartolafc.globo.com/atletas/pontuados'
+const parciaisJogadores = {}
+
+axios.get(urlParciais)
+    .then(async response => {
+        const parciais = response.data.atletas
+        Object.keys(parciais).forEach(jogador => {
+            // console.log(jogador, parciais[jogador]['apelido'] ,parciais[jogador]['pontuacao'])
+            // parciaisJogadores[jogador] = parciais[jogador]['pontuacao']
+            let ptsParciais = parciais[jogador]['pontuacao']
+            console.log(`${jogador};${ptsParciais.toString().replace('.', ',')}`)
+        })
+    })
